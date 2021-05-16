a869d2fbed7ff5b20ae218dadee6dfc9567760d2
Furkan Işıtan <furkanisitan96@gmail.com>
2021-05-17 00:40:57 +0300

    Fix Character class package import in CharacterRepository

--- src/main/java/com/furkanisitan/moviecatalog/dataacces/abstracts/CharacterRepository.java
+++ src/main/java/com/furkanisitan/moviecatalog/dataacces/abstracts/CharacterRepository.java
@@ -1,5 +1,6 @@
 package com.furkanisitan.moviecatalog.dataacces.abstracts;
 
+import com.furkanisitan.moviecatalog.entities.concretes.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
