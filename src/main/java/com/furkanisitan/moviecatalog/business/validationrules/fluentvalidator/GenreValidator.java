28675bfb0f0e8f67970bd8cd1d75ba28c0c8bd70
Furkan Işıtan <furkanisitan96@gmail.com>
2021-05-17 19:28:01 +0300

    Add GenreValidator and unit test

---
+++ src/main/java/com/furkanisitan/moviecatalog/business/validationrules/fluentvalidator/GenreValidator.java
@@ -0,0 +1,19 @@
+package com.furkanisitan.moviecatalog.business.validationrules.fluentvalidator;
+
+import br.com.fluentvalidator.AbstractValidator;
+import com.furkanisitan.moviecatalog.entities.concretes.Genre;
+import org.springframework.stereotype.Component;
+
+import static br.com.fluentvalidator.predicate.LogicalPredicate.not;
+import static br.com.fluentvalidator.predicate.StringPredicate.stringEmptyOrNull;
+
+
+@Component
+public class GenreValidator extends AbstractValidator<Genre> {
+
+    @Override
+    public void rules() {
+        ruleFor(Genre::getName)
+                .must(not(stringEmptyOrNull()));
+    }
+}
---
+++ src/test/java/com/furkanisitan/moviecatalog/business/validators/GenreValidatorUnitTests.java
@@ -0,0 +1,32 @@
+package com.furkanisitan.moviecatalog.business.validators;
+
+import com.furkanisitan.moviecatalog.business.validationrules.fluentvalidator.GenreValidator;
+import com.furkanisitan.moviecatalog.core.validation.fluentvalidator.FluentValidatorException;
+import com.furkanisitan.moviecatalog.core.validation.fluentvalidator.ValidatorTool;
+import com.furkanisitan.moviecatalog.entities.concretes.Genre;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class GenreValidatorUnitTests {
+
+    private GenreValidator genreValidator;
+
+    @BeforeAll
+    public void init() {
+        genreValidator = new GenreValidator();
+    }
+
+    @Test
+    public void test_NameEmptyOrNull_ThrowFluentValidatorException() {
+
+        var genre = Genre.builder().name("").build();
+
+        Assertions.assertThrows(FluentValidatorException.class, () -> {
+            ValidatorTool.validate(genreValidator, genre);
+        });
+    }
+
+}
